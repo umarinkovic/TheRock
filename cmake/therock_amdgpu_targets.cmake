7548051578c3a46e9cfb2feacf42fc5b2335fe3b
Scott Todd <scott.todd0@gmail.com>
2025-09-04 08:31:39 -0700

    Add product name to gfx950 target. (#1387)
    Already documented here:
    https://github.com/ROCm/TheRock/blob/a880e83ac1861c3d5a2a33e1bcc6be55efe8ab21/RELEASES.md?plain=1#L89-L92

--- cmake/therock_amdgpu_targets.cmake
+++ cmake/therock_amdgpu_targets.cmake
@@ -54,7 +54,7 @@ therock_add_amdgpu_target(gfx90a "MI210/250 CDNA" FAMILY gfx90X-all dcgpu-all gf
 therock_add_amdgpu_target(gfx942 "MI300A/MI300X CDNA" FAMILY dcgpu-all gfx94X-all gfx94X-dcgpu)
 
 # gfx950
-therock_add_amdgpu_target(gfx950 "" FAMILY dcgpu-all gfx950-all gfx950-dcgpu)
+therock_add_amdgpu_target(gfx950 "MI350X/MI355X CDNA" FAMILY dcgpu-all gfx950-all gfx950-dcgpu)
 
 # gfx101X family
 therock_add_amdgpu_target(gfx1010 "AMD RX 5700" FAMILY dgpu-all gfx101X-all gfx101X-dgpu
