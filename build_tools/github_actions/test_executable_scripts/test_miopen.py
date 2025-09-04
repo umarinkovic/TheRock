35444a39ea656b0a77097c1d0ed84e5abfadf06c
Vsevolod Golovko <vsevolod.golovko@amd.com>
2025-09-04 10:41:00 +0200

    [MIOpen] Adding the rest of the tests to TheRock from MIOpen's gtest folder (#1248)
    A lor of tests were added.
    However, some of them are failing, so I had to disable them:
    negative_filter.append("*/GPU_KernelTuningNetTest*")
    negative_filter.append("*DBSync*")
    negative_filter.append("*/GPU_MIOpenDriver*")
    
    ---------
    
    Co-authored-by: Vsevolod Golovko <gvsevolo@amd.com>

--- build_tools/github_actions/test_executable_scripts/test_miopen.py
+++ build_tools/github_actions/test_executable_scripts/test_miopen.py
@@ -24,6 +24,7 @@ positive_filter.append("*/GPU_BNOCLBWD*_*")
 positive_filter.append("*/GPU_BNFWD*_*")
 positive_filter.append("*/GPU_BNOCLFWD*_*")
 positive_filter.append("*/GPU_BNInfer*_*")
+positive_filter.append("*/GPU_BNActivInfer_*")
 positive_filter.append("*/GPU_BNOCLInfer*_*")
 positive_filter.append("*/GPU_bn_infer*_*")
 
@@ -42,14 +43,62 @@ positive_filter.append("*/GPU_conv*")
 # Solvers
 positive_filter.append("*/GPU_UnitTestConv*")
 
-negative_filter.append("*DBSync*")
+# Misc
+
+positive_filter.append("*/GPU_Dropout*")
+positive_filter.append("*/GPU_GetitemBwd*")
+positive_filter.append("*/GPU_GLU_*")
+
+positive_filter.append("*/GPU_Bwd_Mha_*")
+positive_filter.append("*/GPU_Fwd_Mha_*")
+
+positive_filter.append("*/GPU_MhaBackward_*")
+positive_filter.append("*/GPU_MhaForward_*")
+positive_filter.append("*/GPU_GroupConv*")
+positive_filter.append("*/GPU_GroupNorm_*")
+positive_filter.append("*/GPU_GRUExtra_*")
+positive_filter.append("*/GPU_TestActivation*")
+positive_filter.append("*/GPU_HipBLASLtGEMMTest*")
+positive_filter.append("*/GPU_KernelTuningNetTestConv*")
+positive_filter.append("*/GPU_Kthvalue_*")
+positive_filter.append("*/GPU_LayerNormTest*")
+positive_filter.append("*/GPU_LayoutTransposeTest_*")
+positive_filter.append("*/GPU_Lrn*")
+positive_filter.append("*/GPU_lstm_extra*")
+
+positive_filter.append("*GPU_TestMhaFind20*")
+positive_filter.append("*/GPU_MultiMarginLoss_*")
+positive_filter.append("*/GPU_ConvNonpack*")
+positive_filter.append("*/GPU_PerfConfig_HipImplicitGemm*")
+positive_filter.append("*/GPU_AsymPooling2d_*")
+positive_filter.append("*/GPU_WidePooling2d_*")
+positive_filter.append("*/GPU_PReLU_*")
+positive_filter.append("*/GPU_Reduce*")
+positive_filter.append("*/GPU_reduce_custom_*")
+positive_filter.append("*/GPU_regression_issue_*")
+positive_filter.append("*/GPU_RNNExtra_*")
+positive_filter.append("*/GPU_RoPE*")
+positive_filter.append("*/GPU_Softmax*")
+positive_filter.append("*/GPU_SoftMarginLoss*")
+positive_filter.append("*/GPU_T5LayerNormTest_*")
+positive_filter.append("*/GPU_Op4dTensorGenericTest_*")
+positive_filter.append("*/GPU_TernaryTensorOps_*")
+positive_filter.append("*/GPU_unaryTensorOps_*")
+positive_filter.append("*/GPU_Transformers*")
+positive_filter.append("*/GPU_TunaNetTest_*")
+positive_filter.append("*/GPU_UnitTestActivationDescriptor_*")
+positive_filter.append("*/GPU_FinInterfaceTest*")
+positive_filter.append("*/GPU_VecAddTest_*")
+
+#############################################
+
 negative_filter.append("*DeepBench*")
 negative_filter.append("*MIOpenTestConv*")
 
-# Temporary fails
-negative_filter.append("*ConvBiasResAddActivation*")
-negative_filter.append("*ConvFwdBiasResAddActiv*")
-negative_filter.append("*GPU_FusionSetArg_FP16*")
+# Failing tests
+negative_filter.append("*/GPU_KernelTuningNetTest*")
+negative_filter.append("*DBSync*")
+negative_filter.append("*/GPU_MIOpenDriver*")
 
 gtest_final_filter_cmd = (
     "--gtest_filter=" + ":".join(positive_filter) + "-" + ":".join(negative_filter)
